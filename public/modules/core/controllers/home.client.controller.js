99a7232864ca5e6cf301ce3db9da99fca1ce0a4b
Seva Zhidkov <zhidkovseva@gmail.com>
2015-10-07 18:05:04 +0400

    Add displaying of users

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,16 +1,21 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', '$http', 'Authentication', 'Users',
+	function($scope, $http, Authentication, Users) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
-
-		if (Authentication.user != '') {
+		if (Authentication.user !== '') {
 			$scope.helloMessage = 'Привет, ' + Authentication.user.displayName;
 		} else {
 			// TODO: Redirect to login page
-			$scope.helloMessage = 'Необходимо залогиниться или создать аккаунт';
+		  $scope.helloMessage = 'Необходимо залогиниться или создать аккаунт';
 		}
+
+		// Get all users
+		$http.get('/users')
+		       .success(function(response) {
+						 $scope.users = response;
+					 });
 	}
 ]);
