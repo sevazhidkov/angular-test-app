6b2d9a10fc5e570c5910e4be67621a18545c0954
Seva Zhidkov <zhidkovseva@gmail.com>
2015-10-07 15:44:07 +0400

    Create hello message for guests

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,12 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		if (Authentication.user != '') {
+			$scope.helloMessage = 'Привет, ' + Authentication.user.displayName;
+		} else {
+			// TODO: Redirect to login page
+			$scope.helloMessage = 'Необходимо залогиниться или создать аккаунт';
+		}
 	}
 ]);
