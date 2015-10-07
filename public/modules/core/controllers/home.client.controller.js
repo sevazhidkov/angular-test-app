a8609d63bb4ad116904be74aa9f1b66f676ae294
Seva Zhidkov <zhidkovseva@gmail.com>
2015-10-07 19:04:32 +0400

    Add redirecting to sing in page if user not authorizated'

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,15 +1,14 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', '$http', 'Authentication', 'Users',
-	function($scope, $http, Authentication, Users) {
+angular.module('core').controller('HomeController', ['$scope', '$http', '$location', 'Authentication', 'Users',
+	function($scope, $http, $location, Authentication, Users) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
 		if (Authentication.user !== '') {
 			$scope.helloMessage = 'Привет, ' + Authentication.user.displayName;
 		} else {
-			// TODO: Redirect to login page
-		  $scope.helloMessage = 'Необходимо залогиниться или создать аккаунт';
+		  $location.path('/signin');
 		}
 
 		// Get all users
