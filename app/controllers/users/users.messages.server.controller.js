36e76f11830cbd450c5da5f9fe8f1f6dcb861208
Seva Zhidkov <zhidkovseva@gmail.com>
2015-10-07 19:28:37 +0400

    Add messages field to server response

--- app/controllers/users/users.messages.server.controller.js
+++ app/controllers/users/users.messages.server.controller.js
@@ -13,7 +13,7 @@ var _ = require('lodash'),
  * View all users
  */
 exports.all = function(req, res) {
-  User.find({}, {displayName: 1}, function(err, docs) {
+  User.find({}, {displayName: 1, messages: 1}, function(err, docs) {
     return res.status(200).send(docs);
   });
 };
